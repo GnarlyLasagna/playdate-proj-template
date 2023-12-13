ddbe6cbb2d0ebfe669b54373452b457a4c971fe8
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2023-12-12 18:18:22 -0600

    Add files via upload

---
+++ src/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/Users/evandolatowski/Developer/PlaydateSDK/C_API/Examples/Hello World/hello_world.pdx"
+  "CMakeFiles/hello_world.dir/main.c.o"
+  "CMakeFiles/hello_world.dir/main.c.o.d"
+  "libhello_world.dylib"
+  "libhello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
